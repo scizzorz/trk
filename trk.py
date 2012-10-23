69eae68a2c723bf71d85c2b86021fd42cd2c27e9
John Weachock <jweachock@gmail.com>
2012-10-22 20:46:48 -0400

    Slightly modified the roadmap and added examples

--- trk.py
+++ trk.py
@@ -2,7 +2,8 @@
 import sys, getopt
 
 '''
-I'm not sure how these doc things work yet. I don't use Python much, but for right now I'm just using it as a basic road map for my ideas.
+I'm not sure how these doc things work yet. I don't use Python much, but for right now I'm just
+using it as a basic road map for my ideas.
 
 Add a new task:
 	trk.py "task"
@@ -27,10 +28,19 @@ Basic task storage/layout:
 	plaintext
 	one task per line
 	priority like this: p3
-	due dates like this: d10/31, d10/31/2012, etc.
-	due time like this: t10:00am, t10am, t10, t3pm, etc.
+	due date like this: d10/31, d10/31/2012, etc.
+	due date + time like this: d{date}@10am, d{date}@8:30pm, d{date}@8:30, d{date}@20, etc.
 	projects like this: +project
 	contexts like this: @context
+	ideally have it limit it to one priority / date / time per task, but we'll see about that
+	no limit to number of projects / contexts it can have
+	examples:
+		p2 d10/22@8pm submit lab 220.2 +cs220 @desktop ## (priority 2, due October 22 at 8pm
+		p2 finish work for Jim +msa @desktop
+		p1 d10/31 make Halloween costume
+		p1 d10/31 buy Halloween costume materials @shopping
+		work on +trk
+		call Mom @phone
 '''
 
 
