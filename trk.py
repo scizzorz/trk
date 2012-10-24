049ccd274a2fc868602c64bc09479300d66264c3
John Weachock <jweachock@gmail.com>
2012-10-24 11:33:55 -0400

    Refactored several functions; reorganized the command logic; enabled adding / completing tasks

--- trk.py
+++ trk.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
-import sys, getopt, md5, re
+import sys, getopt, md5, re, fileinput
 
 '''
 I'm not sure how these doc things work yet. I don't use Python much, but for right now I'm just
 using it as a basic road map for my ideas.
 
 Add a new task:
+(alias for trk.py add "task")
 	trk.py "task"
 
+Add multiple new tasks:
+	trk.py add "task1" "task2"
+
 Complete a task:
-	trk.py finish|complete|delete|hide "some sort of ID / regex?"
+	trk.py x|finish|complete|hide "taskid"
 
 List tasks:
 (alias for trk.py xregex "^x ")
@@ -23,19 +27,19 @@ List completed tasks:
 	trk.py x|completed|finished|hidden
 
 List tasks assigned to a +project:
-(just an alias for trk.py search "+project")
+(alias for trk.py search "+project")
 	trk.py +project
 
 List tasks assigned to a @context:
-(just an alias for trk.py search "@context")
+(alias for trk.py search "@context")
 	trk.py @context
 
 List tasks given a priority:
-(just an alias for trk.py search "p#")
+(alias for trk.py search "p#")
 	trk.py p#
 
 Search tasks:
-	trk.py search|find "search term"
+	trk.py search|find|se|fi "search term"
 
 Search tasks with regex:
 	trk.py regex|re "pattern"
@@ -63,62 +67,90 @@ Basic task storage/layout:
 		call Mom @phone
 '''
 
-def listLines(todoFile, match='',regex=False):
-	for line in todoFile:
+ID_SIZE=4
+
+def lineid(line):
+	line=line.strip()
+	if line[0:2]=='x ':
+		line=line[2:]
+	return md5.new(line).hexdigest()[0:ID_SIZE]
+	
+def readLines(filename, match='',regex=None):
+	temp=open(filename,'r+')
+	for line in temp:
 		if regex==True and re.search(match,line)!=None:
-			printLine(line.strip())
+			printLine(line)
 		elif regex==False and re.search(match,line)==None:
-			printLine(line.strip())
+			printLine(line)
 		elif match in line:
-			printLine(line.strip())
+			printLine(line)
 
 def printLine(line):
-	lineid = md5.new(line)
-	print "[%s] %s" % (lineid.hexdigest()[0:8], line)
+	line=line.strip()
+	print "[%s] %s" % (lineid(line), line)
+
+def writeLines(filename,lines):
+	temp=open(filename,'a')
+	for i in lines:
+		print "Added [%s] %s" % (lineid(i),i)
+		temp.write('%s\n' % i)
+
+def markLines(filename,match=''):
+	for line in fileinput.input(filename,inplace=1):
+		line=line.strip()
+		if match in lineid(line):
+			# now how do I print that it was actually marked...?
+			print line.replace(line,'x %s' % line)
+		else:
+			print line
 
 def main(argv):
 	task='none'
 	filename='.todo'
-	todoFile=open(filename,'r+')
 
-	if len(argv)>0:
-		if argv[0] in ('finish','complete','delete'):
+	if len(argv)>1: # more than one argument
+		if argv[0] in ('x','finish','complete','hide'):
 			task=argv[1]
 			print "Mark '%s' complete / hidden" % task
-		elif argv[0] in ('search','find'):
+			markLines(filename,task)
+		elif argv[0] in ('se','fi','search','find'):
 			task=argv[1]
 			print "Search '%s'" % task
-			listLines(todoFile,task)
+			readLines(filename,task)
 		elif argv[0] in ('regex','re'):
 			task=argv[1]
 			print "RegEx search '%s'" % task
-			listLines(todoFile,task,True)
+			readLines(filename,task,True)
 		elif argv[0] in ('xregex','xre'):
 			task=argv[1]
 			print "Exclusive RegEx search '%s'" % task
-			listLines(todoFile,task,False)
+			readLines(filename,task,False)
+		elif argv[0] in ('add'):
+			print "Batch add %s" % argv[1:]
+			writeLines(filename,argv[1:])
+	elif len(argv)==1: # only one argument, probably an alias
+		task=argv[0]
+		if task[0]=='@':
+			print "List context '%s'" % task[1:]
+			readLines(filename,task)
+		elif task[0]=='+':
+			print "List project '%s'" % task[1:]
+			readLines(filename,task)
+		elif task[0]=='p':
+			print "List priority '%s'" % task[1:]
+			readLines(filename,task)
 		elif argv[0] in ('x','completed','finished','hidden'):
 			print "List completed tasks"
-			listLines(todoFile,'^x ',True)
+			readLines(filename,'^x ',True)
 		elif argv[0] in ('all'):
 			print "List all tasks"
-			listLines(todoFile)
-		else:
-			task=argv[0]
-			if task[0]=='@':
-				print "List context '%s'" % task[1:]
-				listLines(todoFile,task)
-			elif task[0]=='+':
-				print "List project '%s'" % task[1:]
-				listLines(todoFile,task)
-			elif task[0]=='p':
-				print "List priority '%s'" % task[1:]
-				listLines(todoFile,task)
-			else:
-				print "Add '%s'" % task
-	else:
-		print 'List things'
-		listLines(todoFile,'^x ',False)
+			readLines(filename)
+		else: # no alias
+			print "Add '%s'" % task
+			writeLines(filename,argv)
+	else: # no arguments
+		print 'List incomplete tasks'
+		readLines(filename,'^x ',False)
 
 if __name__=='__main__':
 	main(sys.argv[1:])
