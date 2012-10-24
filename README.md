e2e3719097ae1e1dd5166721ed892089886ed236
John Weachock <jweachock@gmail.com>
2012-10-24 17:11:18 -0400

    Uses an absolute path to the user's home directory instead of the local directory; moved roadmap to the README (oh god am I doing this right)

--- README.md
+++ README.md
@@ -1,4 +1,77 @@
-trk
-===
+# trk
 
-CLI task / micro-notepad
+A nice little command-line interface for organizing tasks and small notes.
+
+## Usage
+
+Add a new task (alias for trk.py add "task"):
+
+	trk.py "task"
+
+Add multiple new tasks:
+
+	trk.py add "task1" "task2"
+
+Complete a task:
+
+	trk.py x|finish|complete|hide "taskid"
+
+List tasks (alias for trk.py xregex "^x "):
+
+	trk.py
+
+List all tasks:
+
+	trk.py all
+
+List completed tasks (alias for trk.py regex "^x "):
+
+	trk.py x|completed|finished|hidden
+
+List tasks assigned to a +project (alias for trk.py search "+project"):
+
+	trk.py +project
+
+List tasks assigned to a @context (alias for trk.py search "@context"):
+
+	trk.py @context
+
+List tasks given a priority (alias for trk.py search "(#)"):
+
+	trk.py #
+
+Search tasks:
+
+	trk.py search|find|se|fi "search term"
+
+Search tasks with regex:
+
+	trk.py regex|re "pattern"
+
+Search tasks with exclusive regex (ie every task that *doesn't* match the pattern):
+
+	trk.py xregex|xre "pattern"
+
+## Basic roadmap
+
+Basic task storage/layout:
+
+* plaintext
+* one task per line
+* priority like this: `(3)` (smaller number is higher priority)
+* due date like this: `[10/31]`, `[10/31/2012]`, etc.
+* due date + time like this: `[11/22@10am]`, `[10/25@8:30pm]`, etc.
+* projects like this: `+project`
+* contexts like this: `@context`
+* finished like this: `x task` (the lowercase x *must* be the first character and *must* be followed by a space!)
+* ideally have it limit it to one priority / date / time per task, but we'll see about that
+* no limit to number of projects / contexts it can have
+
+### Examples
+
+	(2) [10/22@8pm] submit lab 220.2 +cs220 @desktop
+	(2) finish work for Jim +msa @desktop
+	(1) [10/31] make Halloween costume
+	(1) [10/31] buy Halloween costume materials @shopping
+	work on +trk
+	call Mom @phone
--- trk.py
+++ trk.py
@@ -1,71 +1,7 @@
 #!/usr/bin/python
 import sys, getopt, md5, re, fileinput
+from os.path import expanduser
 
-'''
-I'm not sure how these doc things work yet. I don't use Python much, but for right now I'm just
-using it as a basic road map for my ideas.
-
-Add a new task:
-(alias for trk.py add "task")
-	trk.py "task"
-
-Add multiple new tasks:
-	trk.py add "task1" "task2"
-
-Complete a task:
-	trk.py x|finish|complete|hide "taskid"
-
-List tasks:
-(alias for trk.py xregex "^x ")
-	trk.py
-
-List all tasks:
-	trk.py all
-
-List completed tasks:
-(alias for trk.py regex "^x ")
-	trk.py x|completed|finished|hidden
-
-List tasks assigned to a +project:
-(alias for trk.py search "+project")
-	trk.py +project
-
-List tasks assigned to a @context:
-(alias for trk.py search "@context")
-	trk.py @context
-
-List tasks given a priority:
-(alias for trk.py search "(#)")
-	trk.py #
-
-Search tasks:
-	trk.py search|find|se|fi "search term"
-
-Search tasks with regex:
-	trk.py regex|re "pattern"
-
-Search tasks with exclusive regex (ie every task that *doesn't* match the pattern):
-	trk.py xregex|xre "pattern"
-
-Basic task storage/layout:
-	plaintext
-	one task per line
-	priority like this: (3) (smaller number is higher priority)
-	due date like this: [10/31], [10/31/2012], etc.
-	due date + time like this: [11/22@10am], [10/25@8:30pm], etc.
-	projects like this: +project
-	contexts like this: @context
-	finished like this: x task (the lowercase x *must* be the first character and *must* be followed by a space!)
-	ideally have it limit it to one priority / date / time per task, but we'll see about that
-	no limit to number of projects / contexts it can have
-	examples:
-		(2) (10/22@8pm) submit lab 220.2 +cs220 @desktop
-		(2) finish work for Jim +msa @desktop
-		(1) (10/31) make Halloween costume
-		(1) (10/31) buy Halloween costume materials @shopping
-		work on +trk
-		call Mom @phone
-'''
 CONFIG=dict()
 
 CONFIG['id_size']=4
@@ -76,6 +12,7 @@ CONFIG['hi_context']=11
 CONFIG['hi_priority']=9
 CONFIG['hi_due']=14
 CONFIG['hi_done']=8
+CONFIG['file']='.todo'
 
 RE_PROJECT=re.compile(r'\s(\+[a-zA-Z0-9]+)')
 RE_CONTEXT=re.compile(r'\s(\@[a-zA-Z0-9]+)')
@@ -150,7 +87,7 @@ def markLines(filename,match=''):
 
 def main(argv):
 	task='none'
-	filename='.todo'
+	filename="%s/%s" % (expanduser("~"),CONFIG['file'])
 
 	if len(argv)>1: # more than one argument
 		if argv[0] in ('x','finish','complete','hide'):
