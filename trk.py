73ab40fb83eeee8fc539f8953e46c977b46dbd04
John Weachock <jweachock@gmail.com>
2012-10-25 01:58:45 -0400

    So many comments everywhere...

--- trk.py
+++ trk.py
@@ -2,20 +2,35 @@
 import sys, os, getopt, md5, re, time, tempfile
 from os.path import expanduser
 
+# configuration
+# hopefully all of these will be ported to a .trkrc
+# and have command-line flags as well
 CONFIG=dict()
 
+# size of the md5 sum substring used as the task id
 CONFIG['id_size']=4
+
+# highlighting on
 CONFIG['hi_on']=True
+
+# highlight colors (ANSI palette) used to highlight each part of a task
 CONFIG['hi_id']=4
 CONFIG['hi_project']=10
 CONFIG['hi_context']=11
 CONFIG['hi_priority']=9
 CONFIG['hi_due']=14
 CONFIG['hi_done']=8
+
+# which file to use
 CONFIG['file']='.todo'
+
+# which character to use for priority
 CONFIG['priority_char']='!'
+
+# which editor to use to edit tasks
 CONFIG['editor']='vim'
 
+# RegExes used to highlight colors
 RE_PROJECT=re.compile(r'(^|\s)(\+\w+)')
 RE_CONTEXT=re.compile(r'(^|\s)(\@\w+)')
 RE_PRIORITY=re.compile(r'\s*(\((\d)\))\s*')
@@ -28,12 +43,16 @@ def linecmp(a,b):
 	# if a < b, return +
 	# if a = b, return 0
 
+	# doneness
+
 	doneMatchA = RE_DONE.search(a)
 	doneMatchB = RE_DONE.search(b)
 	if doneMatchA!=None and doneMatchB==None:
 		return 1
 	elif doneMatchA==None and doneMatchB!=None:
 		return -1
+
+	# priority
 
 	priorityMatchA = RE_PRIORITY.search(a)
 	priorityMatchB = RE_PRIORITY.search(b)
@@ -46,8 +65,13 @@ def linecmp(a,b):
 		if ret!=0:
 			return ret
 
+
+	# dates
+	
 	dateMatchA = RE_DUE.search(a)
 	dateMatchB = RE_DUE.search(b)
+
+	# convert dateMatchA into a sortable Unix time
 	if dateMatchA != None:
 		monthA=dateMatchA.group(2)
 		dayA=dateMatchA.group(3)
@@ -65,6 +89,7 @@ def linecmp(a,b):
 			pamA='PM'
 		timeA=time.mktime(time.strptime("%s %s %s %s %s %s" % (monthA,dayA,yearA,hourA,minuteA,pamA),"%m %d %Y %I %M %p"))
 
+	# convert dateMatchB into a sortable Unix time
 	if dateMatchB != None:
 		monthB=dateMatchB.group(2)
 		dayB=dateMatchB.group(3)
@@ -82,6 +107,7 @@ def linecmp(a,b):
 			pamB='PM'
 		timeB=time.mktime(time.strptime("%s %s %s %s %s %s" % (monthB,dayB,yearB,hourB,minuteB,pamB),"%m %d %Y %I %M %p"))
 	
+	# sort dateMatches
 	if dateMatchA == None and dateMatchB != None:
 		return 1
 	elif dateMatchA != None and dateMatchB == None:
@@ -91,8 +117,11 @@ def linecmp(a,b):
 		if ret!=0:
 			return -ret/abs(ret)
 
+
+	# string order
 	return cmp(a,b)
 
+# sorting key class
 class K(object):
 	def __init__(self,obj,*args):
 		self.obj=obj
@@ -109,11 +138,13 @@ class K(object):
 	def __ne__(self,other):
 		return linecmp(self.obj,other.obj) != 0
 
+# computes the md5 task id of a line
 def lineid(line):
 	line=line.strip()
 	line=RE_DONE.sub('',line)
 	return md5.new(line).hexdigest()[0:CONFIG['id_size']]
 
+# highlights a string with the given color
 def hi(string,color):
 	if(CONFIG['hi_on']!=True):
 		return string
@@ -122,6 +153,7 @@ def hi(string,color):
 	else:
 		return "\033[%dm%s\033[0m" % (color+82,string)
 	
+# read tasks
 def readLines(filename, match='',regex=None):
 	temp=open(filename,'r')
 	lines=[line for line in temp if line.strip()]
@@ -136,10 +168,12 @@ def readLines(filename, match='',regex=None):
 		elif match in line:
 			print formatLine(line)
 
+# format a line for printing
 def formatLine(line):
 	line=line.strip()
 	preColorLine=line
 
+	# priority
 	matchPriority=RE_PRIORITY.search(line)
 	if matchPriority!=None:
 		priority=hi(CONFIG['priority_char'] * int(matchPriority.group(2)),CONFIG['hi_priority'])+' '
@@ -147,11 +181,13 @@ def formatLine(line):
 		priority=''
 
 
+	# highlighting subs
 	line=RE_PROJECT.sub('\g<1>'+hi('\g<2>',CONFIG['hi_project']),line)
 	line=RE_CONTEXT.sub('\g<1>'+hi('\g<2>',CONFIG['hi_context']),line)
 	line=RE_PRIORITY.sub('',line)
 	line=RE_DUE.sub(hi('\g<0>',CONFIG['hi_due']),line)
 
+	# print them so they're aligned nicely
 	if RE_DONE.search(line)!=None:
 		line=RE_DONE.sub('',line)
 		line=hi("x",CONFIG['hi_done'])+" "+priority+line.strip()
@@ -160,6 +196,7 @@ def formatLine(line):
 
 	return "%s %s" % (hi("["+lineid(preColorLine)+"]",CONFIG['hi_id']),line)
 
+# write lines to the file
 def writeLines(filename,lines):
 	temp=open(filename,'a')
 	for line in lines:
@@ -167,6 +204,8 @@ def writeLines(filename,lines):
 		temp.write('%s\n' % line)
 	temp.close()
 
+# mark lines as complete
+# also used to edit lines
 def markLines(filename,match='',edit=None):
 	temp=open(filename,'r')
 	lines=[line for line in temp if line.strip()]
@@ -176,13 +215,19 @@ def markLines(filename,match='',edit=None):
 	temp=open(filename,'w')
 	for line in lines:
 		line=line.strip()
+
+		# if we're editing, we don't care if it's done or not
 		if edit and match in lineid(line):
 			line=editLine(line)
 			print "Saving new line %s" % formatLine(line)
 			temp.write('%s\n' % line)
+
+		# if we're just marking it, we need to make sure it's not already marked
 		elif match in lineid(line) and RE_DONE.search(line)==None:
 			print "Marking line %s done" % hi('['+lineid(line)+']',CONFIG['hi_id'])
 			temp.write('x %s\n' % line)
+
+		# none
 		else:
 			temp.write('%s\n' % line)
 	temp.close()
@@ -193,18 +238,23 @@ def editLine(line):
 
 	(fd, name) = tempfile.mkstemp(prefix='trk-editor-',suffix='.txt',text=True)
 	try:
+		# open the temp file and fill it up with the existing text
 		f=os.fdopen(fd,'w')
 		f.write(line)
 		f.close()
 
+		# open the editor
 		os.system("%s \"%s\"" % (CONFIG['editor'],name))
 
+		# open the file and read the new text
 		f=open(name)
 		t=f.read()
 		f.close()
 	finally:
+		# remove the temp file
 		os.unlink(name)
 	
+	# return new text
 	return t
 
 def main(argv):
